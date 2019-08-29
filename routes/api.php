ff31d700cbb2d8a815ac63769bfc606a56ed346f
ed vieira <edfelip@gmail.com>
2019-08-29 06:33:31 -0300

    first routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$routes= [
+    '/produtos' => 'API\ProdutoController',
+    '/pedidos' => 'API\PedidoController',
+];
+Route::resources($routes);
