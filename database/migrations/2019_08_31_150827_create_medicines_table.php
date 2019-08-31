63cc1c1e55cba52047f01187db841fe89b734e77
Glauber Mota <glaubernm@gmail.com>
2019-08-31 16:18:05 -0300

    Adiciona modelo de medicamentos

---
+++ database/migrations/2019_08_31_150827_create_medicines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ggrem')->unique();
+            $table->string('principio_ativo');
+            $table->string('nome');
+            $table->string('laboratorio');
+            $table->string('apresentacao');
+            $table->unsignedDecimal('valor_unitario');
+            $table->integer('estoque_inicial');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicines');
+    }
+}
