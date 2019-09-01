b3b18a92e9fe8f701ddcce6e1b72b6b40d6dd1f7
Glauber Mota <glaubernm@gmail.com>
2019-08-31 23:35:57 -0300

    Atributo avatar referente à imagem no Model de medicamento

--- app/Medicine.php
+++ app/Medicine.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Medicine extends Model
 {
-    //
+    /**
+     * Procura por imagens de avatar na storage.
+     *
+     * @return string
+     */
+    public function getAvatarAttribute() : string
+    {
+        $fileFormats = ['.jpg', '.png'];
+        foreach($fileFormats as $fileFormat) {
+            if(Storage::disk('local')->exists($this->ggrem . $fileFormat)) {
+                return $this->ggrem . $fileFormat;
+            }
+        }
+        
+        return '';
+    }
 }
