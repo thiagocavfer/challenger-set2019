cda09873f14f53a4ef4eaf5344813f084ce1f829
Glauber Mota <glaubernm@gmail.com>
2019-09-01 10:25:30 -0300

    Funcionalidade de filtro de resultados

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Medicine;
 use App\Http\Resources\MedicineCollection;
 
 class MedicineController extends Controller
 {
-    public function show()
-    {
+    public function show(Request $request) {
+        if($request->query('filtro')) {
+            $filter = $request->query('filtro');
+
+            return new MedicineCollection(
+                Medicine::query()
+                    ->where('nome', 'LIKE', "%{$filter}%")
+                    ->where('principio_ativo', 'LIKE', "%{$filter}%")
+                    ->where('laboratorio', 'LIKE', "%{$filter}%")
+                    ->get()
+            );
+        }
+
         return new MedicineCollection(Medicine::all());
     }
 }
