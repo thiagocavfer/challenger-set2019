2697fd3a812e2fe716c0953fda718b38b61d7094
Glauber Mota <glaubernm@gmail.com>
2019-09-01 21:23:40 -0300

    Início da funcionalidade de reserva no front-end

--- resources/js/components/Medicine/index.js
+++ resources/js/components/Medicine/index.js
@@ -1,45 +1,108 @@
 import React from 'react';
 
-export default function Medicine(props) {
-  return (
-    <div className="card">
-      { props.image && 
-        <img
-          src={`/storage/${props.image}`}
-          alt=""
-          className="img-fluid"
-        />
-      }
-      <div className="card-body">
-        <h2 className="h3 card-title">
-          {props.heading}
-          <br/>
-          <small className="text-muted">{props.subheading}</small>
-        </h2>
-        <p>{props.text}</p>
-        <span>R$ {props.price}</span>
-      </div>
-      <div className="card-footer">
-        <div className="input-group">
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Quantidade"
-            aria-label="Quantidade"
-            aria-describedby="add-button"
+export default class FilterForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: true,
+      quantity: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.resetState = this.resetState.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.value && event.target.value > 0) {
+      this.setState({
+        disabled: false,
+        quantity: event.target.value
+      });
+    } else {
+      this.resetState();
+    }
+  }
+
+  handleClick(event) {
+    const medicine = {
+      nome: this.props.heading,
+      laboratorio: this.props.subheading,
+      valor_unitario: this.props.price,
+      quantidade: this.state.quantity
+    };
+
+    const oldStorageValue = JSON.parse(localStorage.getItem('medicamentos'));
+    const value = oldStorageValue
+      ? oldStorageValue.concat(medicine)
+      : [medicine];
+
+    localStorage.setItem('medicamentos', JSON.stringify(value));
+
+    alert('Medicamento reservado com sucesso!');
+    
+    this.resetState();
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+  }
+
+  resetState() {
+    this.setState({
+      disabled: true,
+      quantity: ''
+    });
+  }
+
+  render() {
+    const disabled = this.state.disabled ? 'disabled' : null;
+
+    return (
+      <div className="card">
+        { this.props.image && 
+          <img
+            src={`/storage/${this.props.image}`}
+            alt=""
+            className="img-fluid"
           />
-          <div className="input-group-append">
-            <button
-              className="btn btn-primary"
-              type="button"
-              id="add-button"
-            >
-              +
-              <span className="sr-only">Reservar</span>
-            </button>
-          </div>
+        }
+        <div className="card-body">
+          <h2 className="h3 card-title">
+            {this.props.heading}
+            <br/>
+            <small className="text-muted">{this.props.subheading}</small>
+          </h2>
+          <p>{this.props.text}</p>
+          <span>R$ {this.props.price}</span>
+        </div>
+        <div className="card-footer">
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <input
+                type="number"
+                min="1"
+                className="form-control"
+                placeholder="Qtde."
+                aria-label="Quantidade"
+                aria-describedby="add-button"
+                value={this.state.quantity}
+                onChange={this.handleChange}
+              />
+              <div className="input-group-append">
+                <button
+                  className='btn btn-primary'
+                  type="button"
+                  id="add-button"
+                  onClick={this.handleClick}
+                  disabled={this.state.disabled}
+                >
+                  Reservar
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
