869035b35ed9881a506c6d9b3aaa165f7ff8d540
Glauber Mota <glaubernm@gmail.com>
2019-09-02 04:38:31 -0300

    Pequena correção no footer

--- resources/js/components/Footer/index.js
+++ resources/js/components/Footer/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer className="container">
-      <hr class="invisible" />
+      <hr className="invisible" />
       <p className="text-center">
         <small className="text-muted">
           Código por Glauber Mota
