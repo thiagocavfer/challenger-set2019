2324466a4e588807dc437aa26ba051664297ab99
Glauber Mota <glaubernm@gmail.com>
2019-09-06 17:44:31 -0300

    Atualiza e melhora a criação do PDF, corrige validação do input

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Order;
 use App\OrderMedicines;
 
 class OrderController extends Controller
 {
+    /**
+     * Cria novo pedido de reserva.
+     *
+     * @param Request $request
+     */
     public function create(Request $request)
     {
         $medicines = $request->input('medicamentos');
@@ -25,35 +31,72 @@ class OrderController extends Controller
                 ]);
             }
 
-            return $this->getPdf($order, $medicines);
+            return $this->buildPdf(
+                $order->codigo,
+                $medicines
+            );
         }
 
         return response('A verificação do input não foi bem-sucedida.', 400);
     }
 
-    private function verifyInput($medicines)
+    /**
+     * Constrói novo PDF.
+     *
+     * @param Order $order
+     * @param array $medicines
+     */
+    private function buildPdf($code, $medicines)
     {
-        return (
-            is_array($medicines)
-            && array_key_exists('nome', $medicines)
-            && array_key_exists('laboratorio', $medicines)
-            && array_key_exists('valor_unitario', $medicines)
-            && array_key_exists('quantidade', $medicines)
-            && $medicines['nome']
-            && $medicines['laboratorio']
-            && $medicines['valor_unitario']
-            && $medicines['quantidade']
+        $options = new Options();
+        $options->set('defaultFont', 'Helvetica')
+            ->set('defaultPaperSize', 'a4')
+            ->set('defaultPaperOrientation', 'landscape')
+            ->set('fontHeightRatio', 1)
+            ->set('isJavascriptEnabled', false);
+        
+        $pdf = new Dompdf($options);
+        $pdf->loadHtml(
+            view(
+                'order',
+                array(
+                    'code' => $code,
+                    'medicines' => $medicines,
+                )
+            )->render()
         );
-    }
-
-    private function getPdf($order, $medicines)
-    {
-        $pdf = App::make('dompdf.wrapper');
-        $pdf->loadView('order', [
-            'codigo' => $order->codigo,
-            'medicines' => $medicines
-        ]);
+        $pdf->render();
 
         return $pdf->stream();
+    }
+
+    /**
+     * Verificação do input do novo pedido de reserva.
+     *
+     * @param array $medicines
+     * @return bool
+     */
+    private function verifyInput($medicines)
+    {
+        if (!is_array($medicines)) {
+            return false;
+        }
+
+        for ($i = 0; $i < count($medicines); $i++) {
+            if (!is_array($medicines[$i])
+                || !array_key_exists('nome', $medicines[$i])
+                || !array_key_exists('laboratorio', $medicines[$i])
+                || !array_key_exists('valor_unitario', $medicines[$i])
+                || !array_key_exists('quantidade', $medicines[$i])
+                || !$medicines[$i]['nome']
+                || !$medicines[$i]['laboratorio']
+                || !$medicines[$i]['valor_unitario']
+                || !$medicines[$i]['quantidade']
+            ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
